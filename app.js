0fd22d706501830099905d03a1b8aa8421c78329
Dimitris Argyroudis <xidatosar@hotmail.gr>
2017-05-27 00:18:21 +0300

    Further progress with i18n. Middleware added, looking at setting language variable on global scale through middleware

--- app.js
+++ app.js
@@ -6,18 +6,17 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
 var i18n = require("i18n");
-
 var index = require('./routes/index');
-
 var app = express();
 
+// app configuration
 i18n.configure({
     // setup some locales - other locales default to en silently
     locales:['en', 'es'],
     // where to store json files - defaults to './locales' relative to modules directory
     directory: __dirname + '/locales',
-    // fall back from Dutch to German
-    fallbacks:{'es': 'en'},
+    // fall back from one language to another if the given one could not be found
+    //fallbacks:{'es': 'en'},
     // you may alter a site wide default locale
     defaultLocale: 'en',
     // watch for changes in json files to reload locale on updates - defaults to false
@@ -39,37 +38,54 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: true, // true = .sass and false = .scss
-  sourceMap: true
+	src: path.join(__dirname, 'public'),
+	dest: path.join(__dirname, 'public'),
+	indentedSyntax: true, // true = .sass and false = .scss
+	sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 // default: using 'accept-language' header to guess language settings
 app.use(i18n.init);
 
+/** 
+ * Middleware to be invoked before each action
+ */
+app.use(function(req, res, next) {
+    // set locale on the same given the url
+    var rxLocal = /^\/(en|es)/i;
+    if(rxLocal.test(req.url)){
+        var arr = rxLocal.exec(req.url);
+        var local=arr[1];
+        // i18n.setLocale(local);
+		req.setLocale(local);
+    }
+    // no need to set default, as it's being set from i18n.configure
+    // move to the next route
+    next();
+});
+
+/* --> Add Middleware to execute before routes <-- */
+
 app.use('/', index);
 
-/** TODO: create middleware to set localle to correct language
- *  and then navigate to the appropriate route
- */
+/* --> Add Middleware to execute after routes <-- */
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ var express = require('express');
 var router = express.Router();
 var i18n = require("i18n");
 
-/* GET home page. */
+/**
+ * Homepage
+*/
 router.get('/:language(|en|es)', function(req, res, next) {
-  var language = req.params.language;
-  req.setLocale(language);
-  res.render('index', { title: 'Express', language: language });
+    // TODO: see how language variable can be set by middleware and be available on all templates
+    // TODO: also try to rewrite rules without the need of the language variable
+    res.render('index', { title: 'Express', language: req.getLocale() });
 });
 
 module.exports = router;
